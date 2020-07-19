ae35d1c29ec0f0dda19e470ebfc65f3b3f2152f5
Samreen Jameel <Samreen Jameel@LAPTOP-STL3HANR>
2020-07-19 16:51:39 +0530

    First Commit

---
+++ Streams/src/Average_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.DoubleStream;
+
+
+public class Average_Numbers {
+	public static void main(String args[]) {
+		List<Integer> dou=Arrays.asList(1,2,3,4,5);
+		double avg=dou.stream().
+		mapToInt(i->i).
+		average().orElse(-1);
+		System.out.println("Average of numbers is: "+avg);
+
+	}
+
+}
